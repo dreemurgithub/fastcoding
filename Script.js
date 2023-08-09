236d227f94a12fa49eb48cfe767883ca137716f4
dreemurgithub <96066405+dreemurgithub@users.noreply.github.com>
2023-08-09 18:19:09 +0700

    finish

--- Script.js
+++ Script.js
@@ -3,6 +3,10 @@ const listNavBlank = document.querySelector("#listNavBlank");
 const menu = document.querySelector("#menu");
 const homeIcon = document.querySelector("#homeIcon");
 const blankIcon = document.querySelector("#blankIcon");
+const parentTab = document.querySelector("#allTab");
+  const ListTab = [...Array.from(parentTab.children)];
+  const ListTextCard = [...Array.from(document.querySelectorAll(".textCard"))];
+
 const state = {
   menu: false,
   screen: window.screen.width,
@@ -41,17 +45,26 @@ function menuOnOff() {
 }
 
 function card(count) {
-  const parentTab = document.querySelector("#allTab");
-  const ListTab = [...Array.from(parentTab.children)];
-  const ListTextCard = [...Array.from(document.querySelectorAll(".textCard"))];
+  
   ListTab[0].style.backgroundColor = "black";
   ListTab[1].style.backgroundColor = "black";
   ListTab[2].style.backgroundColor = "black";
   ListTextCard[0].style.display = "none";
   ListTextCard[1].style.display = "none";
   ListTextCard[2].style.display = "none";
+
   ListTextCard[count].style.display = "block";
+
+  ListTextCard[0].style.opacity = "0";
+  ListTextCard[1].style.opacity = "0";
+  ListTextCard[2].style.opacity = "0";
+  
+  // wait to the end of the function to stop to change style for transition
+  setTimeout(()=>animate(count),0) 
+}
+function animate(count){
   ListTab[parseInt(count)].style.backgroundColor = "#ff8b38";
+  ListTextCard[count].style.opacity = "1";
 }
 
 card(0);
@@ -59,6 +72,6 @@ card(0);
 
 window.addEventListener("resize", ()=>{
     clearTimeout(timeoutId);
-    timeoutId = setTimeout(menuOnOff,1000)
+    timeoutId = setTimeout(menuOnOff,300)
 });
 menuOnOff()
