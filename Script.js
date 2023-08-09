32b922e72cb94484553f9423ea7c6ebfe83681a4
dreemurgithub <96066405+dreemurgithub@users.noreply.github.com>
2023-08-09 07:08:38 +0700

    finish responsive navbar

--- Script.js
+++ Script.js
@@ -1,40 +1,64 @@
 const listNav = document.querySelector("#listNav");
 const listNavBlank = document.querySelector("#listNavBlank");
-const menu = document.querySelector("#menu")
-const homeIcon = document.querySelector("#homeIcon")
+const menu = document.querySelector("#menu");
+const homeIcon = document.querySelector("#homeIcon");
+const blankIcon = document.querySelector("#blankIcon");
+const state = {
+  menu: false,
+  screen: window.screen.width,
+};
+let timeoutId;
+
 function menuOnOff() {
+  state.menu = !state.menu;
+  state.screen = window.screen.width;
+  if (state.screen > 1200) {
+    menu.style.display = "none";
+    listNav.style.display = "flex";
+    listNavBlank.style.display = "none";
+    homeIcon.style.display = "block";
+    blankIcon.style.display = "none";
 
-    if (listNav.style.display === "none" || listNav.style.display === "") {
-        listNav.style.display = "flex";
-        homeIcon.style.visibility = 'hidden'
-        listNavBlank.style.display = 'none'
-
-        // homeIcon.style.visibility = 'hidden'
-        return;
-    }
+    return;
+  }
+  if (state.screen <= 1200 && !state.menu) {
+    menu.style.display = "block";
+    listNav.style.display = "flex";
+    homeIcon.style.display = "none";
+    blankIcon.style.display = "block";
+    listNavBlank.style.display = "none";
+    return;
+  }
+  if (state.screen <= 1200 && state.menu) {
+    menu.style.display = "block";
     listNav.style.display = "none";
-    listNavBlank.style.display = 'flex'
-    homeIcon.style.visibility = 'visible'
-    //   homeIcon.style.visibility = 'visible'
+    homeIcon.style.display = "block";
+    blankIcon.style.display = "none";
+    listNavBlank.style.display = "flex";
+    return;
+  }
+
 }
 
 function card(count) {
-    const parentTab = document.querySelector("#allTab");
-    const ListTab = [...Array.from(parentTab.children)];
-    const ListTextCard = [...Array.from(document.querySelectorAll(".textCard"))];
-    ListTab[0].style.backgroundColor = "black";
-    ListTab[1].style.backgroundColor = "black";
-    ListTab[2].style.backgroundColor = "black";
-    ListTextCard[0].style.display = "none";
-    ListTextCard[1].style.display = "none";
-    ListTextCard[2].style.display = "none";
-    ListTextCard[count].style.display = "block";
-    ListTab[parseInt(count)].style.backgroundColor = "#ff8b38";
+  const parentTab = document.querySelector("#allTab");
+  const ListTab = [...Array.from(parentTab.children)];
+  const ListTextCard = [...Array.from(document.querySelectorAll(".textCard"))];
+  ListTab[0].style.backgroundColor = "black";
+  ListTab[1].style.backgroundColor = "black";
+  ListTab[2].style.backgroundColor = "black";
+  ListTextCard[0].style.display = "none";
+  ListTextCard[1].style.display = "none";
+  ListTextCard[2].style.display = "none";
+  ListTextCard[count].style.display = "block";
+  ListTab[parseInt(count)].style.backgroundColor = "#ff8b38";
 }
 
 card(0);
 // run to change color 1st load
 
-window.addEventListener('resize', () => {
-    if (listNav.style.display === "none" || listNav.style.display === "") menuOnOff()
-})
+window.addEventListener("resize", ()=>{
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(menuOnOff,1000)
+});
+menuOnOff()
